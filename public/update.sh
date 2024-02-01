faaa7e3cec88caf058d973f049484d8a53a01df1
Marc Horst <info@marc-horst.de>
2024-02-01 21:35:34 +0100

    Update script

--- public/update.sh
+++ public/update.sh
@@ -1,4 +1,10 @@
-echo "Update ..."
+echo "Set Safe dir..."
+git config --system --add safe.directory '*'
+echo "Change dir"
+cd /var/www
+echo "Reset"
 git reset --hard
+echo "Update"
 git pull origin main
-php ../composer.phar install
+echo "Run composer"
+php composer.phar install
