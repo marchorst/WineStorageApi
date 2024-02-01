3c351141dd186e63bc67280e210b84a13f03ee9d
Marc Horst <info@marc-horst.de>
2024-02-01 14:29:24 +0100

    System update menu

---
+++ src/Controller/SystemController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaValidator;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class SystemController extends AbstractController
+{
+    #[Route('/update', name: 'systemupdate_index')]
+    public function index(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $outputs=null;
+        $retval=null;
+        exec('git reset --hard', $output, $retval);
+        $outputs[] = $output;
+        exec('git pull origin main', $output, $retval);
+        $outputs[] = $output;
+        exec('composer install', $output, $retval);
+        $outputs[] = "Returned with status $retval and output:\n";
+        return $this->json($outputs);
+    }
+}
